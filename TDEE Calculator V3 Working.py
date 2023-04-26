06f1c22dda141a5a295d837cca39fcb3eb68f10c
NMason1990 <Nathanielmason90@gmail.com>
2023-04-26 19:26:20 -0400

    fixed lowercase issue and called with f strings

--- TDEE Calculator V3 Working.py
+++ TDEE Calculator V3 Working.py
@@ -1,11 +1,16 @@
-#inputs and functions for tdee
 import math
+import builtins
+
+# override the input() function to convert all input strings to lowercase
+def input(prompt):
+    return builtins.input(prompt).lower()
+
 def main():
-    sex = input("Are you Male or Female?").lower
-    if sex == "Male":
+    sex = input("Are you Male or Female?")
+    if sex == "male":
         print(bmr_male() * tdee())    
     else:
-       print(bmr_female() * tdee())
+        print(bmr_female() * tdee())
 
 #weight in kg, height in cm
 #function for male bmr
@@ -14,6 +19,7 @@ def bmr_male():
     weight = float(input("What is your weight in kg?"))
     height = float(input("What is your height in cm?"))
     return 10 * weight + 6.25 * height - 5 * age + 5
+
 #weight in kg, height in cm
 #function for female bmr
 def bmr_female():
@@ -21,7 +27,8 @@ def bmr_female():
     weight = float(input("What is your weight in kg?"))
     height = float(input("What is your height in cm?"))
     return 10 * weight + 6.25 * height - 5 * age - 161
-# asking for activity level and using if statments to choose what those options represent.
+
+# asking for activity level and using if statements to choose what those options represent.
 def tdee():
     activness = float(input("Choose activity level from the following.\nFor sedentary - desk job and little to no exercise enter 1.\n\
     For light - exercise 1 to 3 days per week enter 2.\nFor moderate - exercise 3 to 5 days per week enter 3.\n\
@@ -38,7 +45,6 @@ def tdee():
         return 1 * 1.9
     
 main()
-
 
 
 
